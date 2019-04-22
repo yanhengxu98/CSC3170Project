5d109b9ac10a5bdd04ac00ccba68286df8ec5b23
yanhengxu98 <yanhengxu98@gmail.com>
2019-04-22 21:13:24 +0800

    Normal update

--- init.py
+++ init.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import *
 
 import sys
 import pymysql
+from datetime import datetime
 
 from database_project import Ui_Form
 
@@ -15,29 +16,53 @@ class MyMainwindow(QMainWindow, Ui_Form):
         super(MyMainwindow, self).__init__(parent)
         self.setupUi(self)
 
-        self.connection = pymysql.connect(host='10.26.1.10', user='root', password='root', db='LGUAirline', port=3306,
+        self.connection = pymysql.connect(host='127.0.0.1', user='root', password='970727', db='LGUAirline', port=3306,
                                           charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
         self.cursor = self.connection.cursor()
-        self.prefix = "select FlightDATE,FlightCode,TakeoffTime,EstArrTime,DepApFCC,ArrApFCC from flight where DepApFCC = SZX"
+        self.prefix = "select FlightDATE,FlightCode,TakeoffTime,EstArrTime,DepApFCC,ArrApFCC from flight where DepApFCC = 'SZX'"
         query_order = self.prefix
         self.cursor.execute(query_order)
         k = 0
-        for i in self.cursor:
-            print("------", i)
+        for attribute in self.cursor:
+            print("------", attribute)
             w = 0
-            for j in i:
+            for j in attribute:
                 # 这里是将int类型转成string类型，方便后面文本设置
-                if type(j) == int:
-                    newItem = QtWidget.QTableWidgetItem(str(j))
-
+                instance = attribute[j]
+                if type(instance) == int:
+                    newItem = QTableWidgetItem(str(instance))
                 else:
-                    newItem = QtWidget.QTableWidgetItem(j)
+                    newItem = QTableWidgetItem(instance)
                 # 根据循环标签一次对table中的格子进行设置
                 self.tableWidget.setItem(k, w, newItem)
                 w += 1
             k += 1
 
-    # def query(self):
+        self.pushButton.clicked.connect(self.query)
+
+    def query(self):
+        self.tableWidget.clearContents()  # 每一次查询时清除表格中信息
+        dep = 'SZX'
+        arr = 'PEK'
+        self.prefix = "select FlightDATE,FlightCode,TakeoffTime,EstArrTime,DepApFCC,ArrApFCC from flight where DepApFCC = '%s' and ArrApFCC = '%s'" % (dep, arr)
+        query_order = self.prefix
+        self.cursor.execute(query_order)
+        k = 0
+        for attribute in self.cursor:
+            print("------", attribute)
+            w = 0
+            for j in attribute:
+                # 这里是将int类型转成string类型，方便后面文本设置
+                instance = attribute[j]
+                if type(instance) == int:
+                    newItem = QTableWidgetItem(str(instance))
+                else:
+                    newItem = QTableWidgetItem(instance)
+                # 根据循环标签一次对table中的格子进行设置
+                self.tableWidget.setItem(k, w, newItem)
+                w += 1
+            k += 1
+
 
 
 if __name__ == '__main__':
